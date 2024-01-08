dd1ba17b729669fc3b1e4ba72596939ad1698ca7
shrinivas <shinivassb16@gmail.com>
2024-01-08 19:48:13 +0530

    updated

--- mutable.sh
+++ mutable.sh
@@ -3,11 +3,11 @@
 #APP_VERSION = $2
 
 #COMPONENT = $3
-env
- aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
-SSH_PASSWORD=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
+ aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
  aws ssm put-parameter  --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
 
-ansible-playbook -i inv expense.yml -e role_name=${COMPONENT}  -e env=${ENV} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD} -e version = ${APP_VERSION}
+SSH_PASSWORD=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
+
+ansible-playbook -i inv expense.yml -e role_name=${COMPONENT}  -e env=${ENV} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD} -e version=${APP_VERSION}
