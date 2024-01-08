fadeb43327308a319938b4d31f0d97b7e2897918
shrinivas <shinivassb16@gmail.com>
2024-01-08 18:04:23 +0530

    updated

--- mutable.sh
+++ mutable.sh
@@ -6,6 +6,8 @@
 env
  aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
- aws ssm put-parameter  --name "${ENV}.${COMPONENT}.app_version"  --type "String"  --value "${APP_VERSION}"  --overwrite
+SSH_PASSWORD=$(aws ssm get-parameter --name ${ssh.password} --with-decryption --query 'Parameter.Value' --output text)
 
-ansible-playbook -i inv expense.yml -e role_name=${COMPONENT}  -e env=${ENV} -e ansible_user=centos -e ansible_password=DevOps321 -e version = ${APP_VERSION}
+ aws ssm put-parameter  --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
+
+ansible-playbook -i inv expense.yml -e role_name=${COMPONENT}  -e env=${ENV} -e ansible_user=centos -e ansible_password=${SSH_PASSWORD} -e version = ${APP_VERSION}
